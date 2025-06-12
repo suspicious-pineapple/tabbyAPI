414628dbbcfe4a3e88999ec4304b3b4a58b94c74
end-me-please <90796271+suspicious-pineapple@users.noreply.github.com>
2025-06-13 01:32:09 +0200

    stugf

--- endpoints/OAI/utils/whisper.py
+++ endpoints/OAI/utils/whisper.py
@@ -1,9 +1,12 @@
 import pathlib
 from fastapi import Request
+from peft import PeftModel
 import torch
 from loguru import logger
 from common.tabby_config import config
 
+from transformers import WhisperFeatureExtractor, WhisperForCausalLM, WhisperForConditionalGeneration, WhisperProcessor, WhisperTokenizer
+#from unsloth import FastModel
 from transformers.pipelines import pipeline
 
 from endpoints.OAI.types.whisper import UsageInfo, WhisperObject, WhisperRequest, WhisperResponse
@@ -11,14 +14,79 @@ from endpoints.OAI.types.whisper import UsageInfo, WhisperObject, WhisperRequest
 
 
 
+#whisper_model = WhisperForConditionalGeneration.from_pretrained("")
+
+#model, tokenizer = FastModel.from_pretrained(
+#    model_name = "D:\models\whisper-vc-merged",
+#    dtype = None, # Leave as None for auto detection
+#    load_in_4bit = False, # Set to True to do 4bit quantization which reduces memory
+#    auto_model = WhisperForConditionalGeneration,
+#    whisper_language = "English",
+#    whisper_task = "transcribe",
+#    # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+#)
+#FastModel.for_inference(model)
+#model.eval()
+
+#model, tokenizer = WhisperForConditionalGeneration.from_pretrained("D:\models\whisper-vc-merged")
+#model, tokenizer = WhisperForConditionalGeneration.from_pretrained("D:\models\whisper-vc-merged")
+
+#processor = WhisperProcessor.from_pretrained("D:\models\whisper-vc-merged")
+
+
+#transcriber = pipeline(
+#    "automatic-speech-recognition",
+#    model=model,
+#    tokenizer=tokenizer,
+#    feature_extractor=model.tokenizer.feature_extractor,
+#    processor=tokenizer,
+#    return_language=True,
+#    torch_dtype=torch.bfloat16  # Remove the device parameter
+#)
+
+
+
+model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-large-v3", low_cpu_mem_usage=True)
+peft_model = PeftModel.from_pretrained(model, "D:\models\whisper-large-v3-lora")
+featureextractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-large-v3")
+merged_model = peft_model.merge_and_unload()
 
 transcriber = pipeline(
+    #model="openai/whisper-large-v3",
+    model=merged_model,
+    tokenizer="openai/whisper-large-v3",
     task="automatic-speech-recognition",
-    model="nyrahealth/CrisperWhisper",
-    #model="D:\models\whisper-large-v3-turbo",
+    feature_extractor=featureextractor, 
     torch_dtype=torch.bfloat16,
     device=0,
 )
+
+#transcriber = pipeline(
+#    model="D:\models\whisper-vc-merged",
+#
+#    task="automatic-speech-recognition", 
+#    torch_dtype=torch.bfloat16,
+#    return_language=True,
+#    #device=0,
+#)
+
+#from transformers import WhisperProcessor
+#processor = WhisperProcessor.from_pretrained("unsloth/whisper-large-v3", task="automatic-speech-recognition")
+#transcriber = pipeline(
+#    "automatic-speech-recognition",
+#    model="unsloth/whisper-large-v3",
+#    tokenizer=processor.tokenizer,
+#    feature_extractor=processor.feature_extractor,
+#    processor=processor,
+#    return_language=True,
+#    torch_dtype=torch.float16  # Remove the device parameter
+#)
+
+
+#transcriber.model.load_adapter(peft_model_id = "D:\models\whisper-vc-lora")
+
+    #model="nyrahealth/CrisperWhisper",
+    #model="D:\models\whisper-large-v3-turbo",
 
 
 #import nemo.collections.asr as nemo_asr
